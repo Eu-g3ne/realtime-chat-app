3f63e67eba20efeed710731be119ef213eb30612
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-09 15:09:27 +0300

    add tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        '100': '100',
+      },
+    },
+    container: {
+      center: true,
+    },
+    screens: {
+      'v-sm': '400px',
+      'sm': '600px',
+      'md': '768px',
+      'lg': '980px',
+      'xl': '1168px',
+      'xxl': '1380px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
