441b92cc5b1f5e45a084aeb80affea8f140ec6ef
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-10 22:35:29 +0300

    add test websocket listening

--- resources/js/views/IndexView.vue
+++ resources/js/views/IndexView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="2xl:container mx-auto">
     <h1>Index View</h1>
+    <div>
+      <button class="m-2 p-2 bg-gray-600 rounded-xl">Press me</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Index",
+  mounted() {
+    Echo.channel("channel").listen(".test", function (e) {
+      console.log(e);
+    });
+  },
 };
 </script>
 
