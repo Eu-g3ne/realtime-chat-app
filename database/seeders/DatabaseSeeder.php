c27087da239fa00b22b595d3e40be381dec81f90
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-12 23:32:36 +0300

    add factories and main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
+use App\Models\Participant;
+use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $user = User::factory()->create();
+    $user2 = User::factory()->create();
+    $thread = Thread::factory()->hasAttached(collect([$user, $user2]))->create();
+    Message::factory()->count(3)->for($user)->for($thread)->create();
+    Message::factory()->count(3)->for($user2)->for($thread)->create();
+  }
 }
