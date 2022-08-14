aa257f3b709dbaf069fe0203d6bb612017e67be0
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-15 00:07:31 +0300

    add axios default settings

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,6 +13,8 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Authorization'] = 'Bearer '+localStorage.getItem('apiToken');
+window.axios.defaults.baseURL = 'http://localhost:8000/api';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
