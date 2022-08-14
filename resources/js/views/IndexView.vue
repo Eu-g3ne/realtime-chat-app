0be52c6d245051f7ac1b96ae0740281c9dc7a22a
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-15 01:58:49 +0300

    change layout

--- resources/js/views/IndexView.vue
+++ resources/js/views/IndexView.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="2xl:container mx-auto">
-    <h1>Index View</h1>
-    <div>
-      <button class="m-2 p-2 bg-gray-600 rounded-xl">Press me</button>
+  <div class="2xl:container mx-auto h-screen background">
+    <div class="flex flex-row h-full gap-0">
+      <v-sidebar class="sm:w-[450px]"></v-sidebar>
+      <div class="grow"><router-view></router-view></div>
     </div>
   </div>
 </template>
 
 <script>
+import vSidebar from "../components/Sidebar.vue";
+
 export default {
+  data: function () {
+    return {};
+  },
   name: "Index",
+  components: { vSidebar },
   mounted() {
-    Echo.channel("channel").listen(".test", function (e) {
-      console.log(e);
-    });
+    // Echo.channel("channel").listen(".test", function (e) {
+    //   console.log(e);
+    // });
   },
 };
 </script>
 
-<style></style>
+<style>
+.background {
+  position: relative;
+  background-color: rgba(255, 255, 100, 0.5);
+}
+.background::before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+  content: "";
+  background-image: url("/img/pattern.svg");
+  background-repeat: repeat;
+}
+</style>
