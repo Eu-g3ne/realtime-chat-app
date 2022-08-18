fc4aefe96a578ec4a3f07ddeaf06df8f6c06dee4
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-19 02:30:15 +0300

    add icons components

---
+++ resources/js/components/icons/InfoIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-12 w-12 bg-blue-300 rounded-full p-2"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
