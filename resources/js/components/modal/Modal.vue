d3e41fe447fa0845c8c6afbc5f2fc4f7a4e022ce
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-23 01:19:33 +0300

    add modal window component

---
+++ resources/js/components/modal/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+  <transition name="fade">
+    <div>
+      <div>
+        <div
+          class="fixed inset-0 w-full h-full bg-black/50 flex justify-center items-center"
+          @click.self="close()"
+        >
+          <div
+            class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full"
+          >
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                <div>
+                  <slot name="icon" />
+                </div>
+                <div
+                  class="mt-3 text-start sm:mt-0 sm:ml-4 sm:text-left sm:grow"
+                >
+                  <slot name="main" />
+                </div>
+              </div>
+            </div>
+            <div
+              class="bg-gray-50 px-4 py-3 sm:px-6 flex flex-col sm:flex-row justify-end gap-2"
+            >
+              <slot name="handler" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-from .fade-enter-to {
+  opacity: 0;
+}
+</style>
