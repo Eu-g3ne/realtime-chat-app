42f62e0c6cde62ed62044fe93affa4c61e63da40
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-26 02:09:41 +0300

    add message actions & context menu

---
+++ resources/js/components/thread/ContextMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="absolute z-100 flex flex-col gap-1 bg-white p-1 rounded-lg"
+    :style="pos"
+    v-click-outside="vcoConfig"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      x: Number,
+      y: Number,
+    },
+  },
+  data() {
+    return {
+      vcoConfig: {
+        events: ["dblclick", "click", "contextmenu"],
+        handler: this.hide,
+      },
+    };
+  },
+  methods: {
+    hide() {
+      this.$emit("hide");
+    },
+  },
+  computed: {
+    pos: function () {
+      return {
+        top: this.position.y + "px",
+        left: this.position.x + "px",
+      };
+    },
+  },
+};
+</script>
+
+<style></style>
--- resources/js/components/thread/ThreadMessage.vue
+++ resources/js/components/thread/ThreadMessage.vue
@@ -2,22 +2,68 @@
   <div
     :class="isOwn"
     class="max-w-[70%] w-fit"
+    @contextmenu.prevent="show($event)"
   >
+    <v-context-menu
+      v-if="isActionsVisible"
+      :position="position"
+      @hide="hide()"
+    >
+      <v-edit-button
+        class="p-1"
+        @click="edit()"
+      >
+        <span class="mr-3">Edit</span>
+        <v-edit-icon />
+      </v-edit-button>
+      <v-danger-button
+        class="p-1"
+        @click="del()"
+      >
+        <span class="mr-3">Delete</span>
+        <v-trash-icon />
+      </v-danger-button>
+    </v-context-menu>
     <div class="rounded-xl bg-green-400 flex justify-between">
-      <span class="p-2 text-black">{{ message.body }}</span>
-      <span class="text-gray-600 text-[12px] place-self-end px-2">{{
-        createdAt
-      }}</span>
+      <span class="p-2 text-black">
+        {{ message.body }}
+      </span>
+      <span class="text-gray-600 text-[12px] place-self-end px-2">
+        {{ createdAt }}
+      </span>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import vTrashIcon from "../icons/TrashIcon.vue";
+import vEditIcon from "../icons/EditIcon.vue";
+import vEditButton from "../buttons/EditButton.vue";
+import vDangerButton from "../buttons/DangerButton.vue";
+
+import vContextMenu from "./ContextMenu.vue";
 
 export default {
+  components: {
+    vContextMenu,
+    vTrashIcon,
+    vEditIcon,
+    vEditButton,
+    vDangerButton,
+  },
   data: function () {
-    return {};
+    return {
+      isActionsVisible: false,
+      position: {
+        x: 0,
+        y: 0,
+      },
+      vcoConfig: {
+        events: ["dblclick", "click", "contextmenu"],
+        handler: this.hide,
+      },
+    };
   },
   props: {
     message: {
@@ -31,6 +77,23 @@ export default {
       },
     },
   },
+  methods: {
+    show(event) {
+      this.position = { x: event.pageX, y: event.pageY };
+      this.isActionsVisible = true;
+    },
+    hide() {
+      this.isActionsVisible = false;
+    },
+    edit() {
+      this.$emit("edit");
+      this.hide();
+    },
+    del() {
+      this.$emit("delete");
+      this.hide();
+    },
+  },
   computed: {
     ...mapGetters("user", ["authenticated"]),
     isOwn() {
@@ -40,7 +103,11 @@ export default {
     },
     createdAt() {
       let date = new Date(this.message.created_at);
-      return `${date.getHours()}:${date.getMinutes()}`;
+      return [date.getHours(), date.getMinutes()]
+        .map((date) => {
+          return String(date).padStart(2, "0");
+        })
+        .join(":");
     },
   },
 };
