77dc0e3fd4393b932fb710016510409c0a840ae8
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-26 02:06:53 +0300

    add blue button

---
+++ resources/js/components/buttons/EditButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    type="button"
+    class="btn-component text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-600"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
