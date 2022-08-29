2529f58c12834800415e6a9fcca99dd9a87ce93e
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-29 23:55:03 +0300

    add: modal window components

---
+++ resources/js/components/modal/ConfirmationModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-modal>
+    <template v-slot:icon>
+      <v-question-icon />
+    </template>
+    <template v-slot:main>
+      <v-title> Are you sure? </v-title>
+    </template>
+    <template v-slot:handler>
+      <v-accept-button @click="$emit('accept')">
+        Accept <v-confirm-icon />
+      </v-accept-button>
+      <v-cancel-button @click="$emit('cancel')"> Cancel </v-cancel-button>
+    </template>
+  </v-modal>
+</template>
+
+<script>
+import vModal from "./Modal.vue";
+import vTitle from "../layout/Title.vue";
+import vAcceptButton from "../buttons/AcceptButton.vue";
+import vCancelButton from "../buttons/CancelButton.vue";
+import vQuestionIcon from "../icons/QuestionIcon.vue";
+import vConfirmIcon from "../icons/ConfirmIcon.vue";
+
+export default {
+  components: {
+    vModal,
+    vTitle,
+    vQuestionIcon,
+    vConfirmIcon,
+    vAcceptButton,
+    vCancelButton,
+  },
+};
+</script>
+
+<style></style>
---
+++ resources/js/components/modal/ThreadEditModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-modal @close="$emit('close')">
+    <template v-slot:icon>
+      <v-edit-icon class="h-12 w-12 bg-blue-300 rounded-full p-2" />
+    </template>
+    <template v-slot:main>
+      <div class="flex flex-col gap-4">
+        <div>
+          <v-title>Thread:</v-title>
+          <v-text-input v-model="thread.name" />
+        </div>
+        <div>
+          <v-users-select :thread="thread" />
+        </div>
+      </div>
+    </template>
+    <template v-slot:handler>
+      <v-danger-button
+        @click="$emit('leave')"
+        class="mr-auto"
+      >
+        Leave
+        <v-leave-icon />
+      </v-danger-button>
+      <v-accept-button @click="$emit('accept')"> Accept </v-accept-button>
+      <v-cancel-button @click="$emit('close')"> Cancel </v-cancel-button>
+    </template>
+  </v-modal>
+</template>
+
+<script>
+import vModal from "./Modal.vue";
+import vAcceptButton from "../buttons/AcceptButton.vue";
+import vCancelButton from "../buttons/CancelButton.vue";
+import vDangerButton from "../buttons/DangerButton.vue";
+import vTextInput from "../forms/inputs/TextInput.vue";
+import vTitle from "../layout/Title.vue";
+import vEditIcon from "../icons/EditIcon.vue";
+import vUsersSelect from "../forms/inputs/UsersSelect.vue";
+import vLeaveIcon from "../icons/LeaveIcon.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    vModal,
+    vAcceptButton,
+    vCancelButton,
+    vDangerButton,
+    vTextInput,
+    vTitle,
+    vEditIcon,
+    vUsersSelect,
+    vLeaveIcon,
+  },
+  data: () => ({
+    nickname: "",
+  }),
+  methods: {
+    initItems() {},
+    updateUsers(value) {
+      console.log(value);
+    },
+  },
+  computed: {
+    ...mapGetters("thread", ["thread"]),
+  },
+};
+</script>
+
+<style></style>
