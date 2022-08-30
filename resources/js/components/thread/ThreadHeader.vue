5bcb337777568c499edca8335714c3f69e78c1dd
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-30 22:56:57 +0300

    add: thread header component

---
+++ resources/js/components/thread/ThreadHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="w-full bg-white shadow-md p-2 cursor-pointer"
+    @click="$emit('click')"
+  >
+    <div class="font-bold text-black text-lg">{{ nameById(id) }}</div>
+    <div class="font-light text-gray-500">{{ membersById(id) }} members</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters("thread", ["nameById", "membersById"]),
+  },
+};
+</script>
+
+<style></style>
